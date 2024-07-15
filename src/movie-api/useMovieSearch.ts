16975f28e513348c8743c2b3ff131104c97cb29a
Kas S <sotimkul@gmail.com>
2024-07-15 18:36:43 +0500

    response from server is captured as QueryResult

--- src/movie-api/useMovieSearch.ts
+++ src/movie-api/useMovieSearch.ts
@@ -1,12 +1,20 @@
 import {useEffect, useState} from "react"
+import {Movie} from "../types";
+
+type QueryResult = {
+    page: number,
+    results: Movie[],
+    total_pages: number,
+    total_results: number
+}
 
 function useMovieSearch() {
     const [query, setQuery] = useState(""),
-          [result, setResult] = useState([])
+          [result, setResult] = useState<QueryResult | undefined>(undefined)
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=89afb4ada1fb6e8592502acb0d5befba`, {method: 'GET', headers: {accept: 'application/json'}})
         .then(res => res.json()).then(data => {
-            setResult(data)
+            setResult(data as QueryResult)
         })
     }, [query])
     return {result, setQuery}
