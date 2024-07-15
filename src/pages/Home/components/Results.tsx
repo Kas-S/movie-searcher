9d215c8b66f71f8a0708d4f8128e7dd2ce5f2243
Kas S <sotimkul@gmail.com>
2024-07-16 00:29:11 +0500

    search results displayed with poster, original title and date

--- src/pages/Home/components/Results.tsx
+++ src/pages/Home/components/Results.tsx
@@ -6,9 +6,11 @@ interface Props {
 
 function SearchResults({results}: Props) {
     return (
-        <ul>
+        <ul className="flex flex-row flex-wrap gap-3 justify-center items-center">
             {results.map((result: Movie) => (
-                <li key={result.id}>{result.original_title} ({result.release_date})</li>
+                <li key={result.id} className="w-[30%]">
+                    <img src={`https://image.tmdb.org/t/p/w500/${result.poster_path}`} alt=""/>
+                    {result.original_title} ({result.release_date.slice(0, 4)})</li>
             ))}
         </ul>
     )
